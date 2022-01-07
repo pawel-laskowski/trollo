41877281afe824af6729c28b8e7e484814acade9
Pawel Laskowski <pawel-laskowski@outlook.com>
2022-01-07 14:20:04 +0100

    Add eslintignore file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,39 @@
 module.exports = {
-    extends: [
-      'airbnb-typescript',
-      'airbnb/hooks',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:jest/recommended',
-      'plugin:prettier/recommended'
-    ],
-    plugins: ['react', '@typescript-eslint', 'jest', 'import'],
-    env: {
-      browser: true,
-      es6: true,
-      jest: true,
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest', 'import'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
       },
-      ecmaVersion: 2018,
-      sourceType: 'module',
-      project: './tsconfig.json',
-    },
-    rules: {
-      'linebreak-style': 'off',
-      'prettier/prettier': [
-        'error',
-        {
-          endOfLine: 'auto',
-        },
-      ],
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off"
-    },
-  };
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
