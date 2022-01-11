17cefd08cc857ea537275d468743daab54784528
Pawel Laskowski <pawel-laskowski@outlook.com>
2022-01-11 19:42:48 +0100

    Correct types for routes children

--- src/routers/PrivateRoute.tsx
+++ src/routers/PrivateRoute.tsx
@@ -1,9 +1,14 @@
+import { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom'
 import { isLoaded, isEmpty } from 'react-redux-firebase'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
 
-export const PrivateRoute = ({ children }: any) => {
+export const PrivateRoute = ({ children }: { children: ReactNode }) => {
   const auth = useSelector((state: RootState) => state.firebase.auth)
-  return isLoaded(auth) && !isEmpty(auth) ? children : <Navigate to="/" />
+  return isLoaded(auth) && !isEmpty(auth) ? (
+    <>{children}</>
+  ) : (
+    <Navigate to="/" />
+  )
 }
--- src/routers/PublicRoute.tsx
+++ src/routers/PublicRoute.tsx
@@ -1,13 +1,14 @@
+import { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom'
 import { isLoaded, isEmpty } from 'react-redux-firebase'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
 
-export const PublicRoute = ({ children }: any) => {
+export const PublicRoute = ({ children }: { children: ReactNode }) => {
   const auth = useSelector((state: RootState) => state.firebase.auth)
   return isLoaded(auth) && !isEmpty(auth) ? (
     <Navigate to="/dashboard" />
   ) : (
-    children
+    <>{children}</>
   )
 }
