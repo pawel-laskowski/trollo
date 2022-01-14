b7bcff74999e6c60eb2da5c4f8a79fb6f03b87eb
Pawel Laskowski <pawel-laskowski@outlook.com>
2022-01-14 09:16:35 +0100

    Test for login and logout functions

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
+import { useFirebase, isLoaded, isEmpty } from 'react-redux-firebase'
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 import { Header } from './Header'
+
+jest.mock('react-redux-firebase')
 
 const mockStore = configureStore()
 const initialState = { firebase: {} }
@@ -10,6 +13,11 @@ const loggedInState = {
   firebase: {
     auth: 'user',
   },
+}
+
+const mockLoggedUser = () => {
+  ;(isLoaded as unknown as jest.Mock).mockImplementation(() => true)
+  ;(isEmpty as unknown as jest.Mock).mockImplementation(() => false)
 }
 
 it('should render Login button if user is logged out', () => {
@@ -24,6 +32,8 @@ it('should render Login button if user is logged out', () => {
 })
 
 it('should render Logout button if user is logged in', () => {
+  mockLoggedUser()
+
   render(
     <Provider store={mockStore(loggedInState)}>
       <MemoryRouter>
@@ -33,3 +43,43 @@ it('should render Logout button if user is logged in', () => {
   )
   expect(screen.getByRole('button')).toHaveTextContent('Logout')
 })
+
+it('should call firebase login function', () => {
+  const login = jest.fn().mockResolvedValue({})
+  ;(useFirebase as jest.Mock).mockReturnValue({ login })
+
+  render(
+    <Provider store={mockStore(initialState)}>
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    </Provider>
+  )
+  fireEvent.click(screen.getByRole('button'))
+  expect(login).toHaveBeenCalled()
+})
+
+it('should call firebase logout function', () => {
+  mockLoggedUser()
+
+  const logout = jest.fn().mockResolvedValue({})
+  ;(useFirebase as jest.Mock).mockReturnValue({ logout })
+
+  render(
+    <Provider store={mockStore(loggedInState)}>
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    </Provider>
+  )
+  fireEvent.click(screen.getByRole('button'))
+  expect(logout).toHaveBeenCalled()
+})
+
+// it('should navigate to "/dashboard" after user logged in', () => {})
+
+// it('should navigate to "/" after user logged out', () => {})
+
+// it('should open alert when error occurs while login', () => {})
+
+// it('should open alert when error occurs while logout', () => {})
