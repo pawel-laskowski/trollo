a1b0d31b8bb8b6874edde0ee2452bcaa990d7250
Paweł Laskowski <43352870+pawel-laskowski@users.noreply.github.com>
2022-01-14 11:58:25 +0100

    Routes in AppRouter divided into public and private  (#3)

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { Dashboard } from '../components/Dashboard'
 import { LoginPage } from '../components/LoginPage'
-
+import { PublicRoute } from './PublicRoute'
+import { PrivateRoute } from './PrivateRoute'
 export const AppRouter = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<PublicRoute children={<LoginPage />} />} />
+      <Route
+        path="/dashboard"
+        element={<PrivateRoute children={<Dashboard />} />}
+      />
     </Routes>
   </BrowserRouter>
 )
---
+++ src/routers/PublicRoute.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react'
+import { Navigate } from 'react-router-dom'
+import { isLoaded, isEmpty } from 'react-redux-firebase'
+import { useSelector } from 'react-redux'
+import { RootState } from '../store/store'
+import { CircularProgress } from '@mui/material'
+
+export const PublicRoute = ({ children }: { children: ReactNode }) => {
+  const auth = useSelector((state: RootState) => state.firebase.auth)
+
+  if (!isLoaded(auth)) {
+    return <CircularProgress />
+  }
+
+  return !isEmpty(auth) ? <Navigate to="/dashboard" /> : <>{children}</>
+}
