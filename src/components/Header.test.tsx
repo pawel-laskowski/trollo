f2c0e13749d8f88827139bf6d422eccf3b067df3
Pawel Laskowski <pawel-laskowski@outlook.com>
2022-01-14 18:45:54 +0100

    Tests for navigate and alert

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { useFirebase, isLoaded, isEmpty } from 'react-redux-firebase'
 import { Provider } from 'react-redux'
@@ -6,6 +6,15 @@ import configureStore from 'redux-mock-store'
 import { Header } from './Header'
 
 jest.mock('react-redux-firebase')
+
+let mockedUseNavigate: jest.Mock
+beforeEach(() => {
+  mockedUseNavigate = jest.fn()
+})
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as object),
+  useNavigate: () => mockedUseNavigate,
+}))
 
 const mockStore = configureStore()
 const initialState = { firebase: {} }
@@ -76,10 +85,79 @@ it('should call firebase logout function', () => {
   expect(logout).toHaveBeenCalled()
 })
 
-// it('should navigate to "/dashboard" after user logged in', () => {})
+it('should navigate to "/dashboard" after user logged in', async () => {
+  const login = jest.fn().mockResolvedValue({})
+  ;(useFirebase as jest.Mock).mockReturnValue({ login })
 
-// it('should navigate to "/" after user logged out', () => {})
+  render(
+    <Provider store={mockStore(initialState)}>
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    </Provider>
+  )
+  fireEvent.click(screen.getByRole('button'))
+  await waitFor(() =>
+    expect(mockedUseNavigate).toHaveBeenLastCalledWith('/dashboard')
+  )
+})
 
-// it('should open alert when error occurs while login', () => {})
+it('should navigate to "/" after user logged out', async () => {
+  mockLoggedUser()
 
-// it('should open alert when error occurs while logout', () => {})
+  const logout = jest.fn().mockResolvedValue({})
+  ;(useFirebase as jest.Mock).mockReturnValue({ logout })
+
+  render(
+    <Provider store={mockStore(loggedInState)}>
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    </Provider>
+  )
+  fireEvent.click(screen.getByRole('button'))
+  await waitFor(() => expect(mockedUseNavigate).toHaveBeenLastCalledWith('/'))
+})
+
+it('should open alert when error occurs while login', async () => {
+  const login = jest.fn().mockRejectedValue(Error())
+  ;(useFirebase as jest.Mock).mockReturnValue({ login })
+
+  render(
+    <Provider store={mockStore(initialState)}>
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    </Provider>
+  )
+
+  fireEvent.click(screen.getByRole('button'))
+
+  await waitFor(() =>
+    expect(screen.getByRole('alert')).toHaveTextContent(
+      'Login error! Please try again.'
+    )
+  )
+})
+
+it('should open alert when error occurs while logout', async () => {
+  mockLoggedUser()
+
+  const logout = jest.fn().mockRejectedValue(Error())
+  ;(useFirebase as jest.Mock).mockReturnValue({ logout })
+
+  render(
+    <Provider store={mockStore(loggedInState)}>
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    </Provider>
+  )
+
+  fireEvent.click(screen.getByRole('button'))
+  await waitFor(() =>
+    expect(screen.getByRole('alert')).toHaveTextContent(
+      'Logout error! Please try again.'
+    )
+  )
+})
