09bf5d3a12142da90cd036164af3b027022e1b39
Pawel Laskowski <pawel-laskowski@outlook.com>
2022-01-14 10:54:31 +0100

    Checking if user data is loaded before navigate

--- src/routers/PublicRoute.tsx
+++ src/routers/PublicRoute.tsx
@@ -3,9 +3,15 @@ import { Navigate } from 'react-router-dom'
 import { isLoaded, isEmpty } from 'react-redux-firebase'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
+import { CircularProgress } from '@mui/material'
 
 export const PublicRoute = ({ children }: { children: ReactNode }) => {
   const auth = useSelector((state: RootState) => state.firebase.auth)
+
+  if (!isLoaded(auth)) {
+    return <CircularProgress />
+  }
+
   return isLoaded(auth) && !isEmpty(auth) ? (
     <Navigate to="/dashboard" />
   ) : (
